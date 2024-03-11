1ec45935c58a3f4facd9f1de3ff55ec7924595e8
Ir200on <snezokdasha@gmail.com>
2024-03-11 15:45:13 +0200

    31homework

---
+++ homework31/index.js
@@ -0,0 +1,13 @@
+// import {name} from "/module";
+// import "./homework.css";
+
+// console.log("Your name: " + name);
+
+// import isEmail from 'validator/lib/isEmail';
+// console.log(isEmail('foo@bar.com')); 
+// var validator = require('validator');
+
+// console.log(validator.isEmail('foo@bar.com')); 
+
+// var moment = require('moment');
+// console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
